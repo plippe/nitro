20b55c100960d542e6bcace4245dd4cc779478f2
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-09 19:02:40 +0100

    Add restrictions for better code

---
+++ project/plugins.sbt
@@ -0,0 +1,2 @@
+addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.1.3")
+addSbtPlugin("com.geirsson" % "sbt-scalafmt" % "1.5.1")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,26 +2,29 @@ package com.github.plippe.nitro
 
 object Main extends App {
 
-    def distance(l: Int) = {
-        Range(1, Int.MaxValue, 2)
-            .find(n => math.pow(n, 2) >= l)
-            .map { n =>
-                val pow = math.pow(n, 2).toInt
+  def sqrt(i: Int): Int = math.pow(i.toDouble, 2).toInt
 
-                val stepsFromCorner = n - 1
-                val stepsToCorner = 0.to(4)
-                    .map(i => pow - (n-1) * i)
-                    .map(c => math.abs(c - l))
-                    .min
+  def distance(l: Int): Option[Int] = {
+    Range(1, Int.MaxValue, 2)
+      .find(n => sqrt(n) >= l)
+      .map { n =>
+        val nn = sqrt(n)
 
-                stepsFromCorner - stepsToCorner
-            }
-    }
+        val stepsFromCorner = n - 1
+        val stepsToCorner = 0
+          .to(4)
+          .map(i => nn - (n - 1) * i)
+          .map(c => math.abs(c - l))
+          .min
 
-    assert(distance(1) == Some(0))
-    assert(distance(12) == Some(3))
-    assert(distance(23) == Some(2))
-    assert(distance(1024) == Some(31))
+        stepsFromCorner - stepsToCorner
+      }
+  }
 
-    println("Hello World")
+  assert(distance(1) == Some(0))
+  assert(distance(12) == Some(3))
+  assert(distance(23) == Some(2))
+  assert(distance(1024) == Some(31))
+
+  println("Hello World")
 }
